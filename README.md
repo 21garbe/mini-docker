359c34f1edf046649d59200f7f2da7bccea90300
Pierre Berranger <prenom.nom@etu.minesparis.psl.eu>
2025-01-27 17:09:12 +0100

    Isolate process and file system

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
-# mini-docker
+# Mini-docker
+
 This project aims at creating a toy docker project for third year in school
+
+## Usage
+
+```bash
+cargo build
+sudo ./target/debug/mini-docker / / echo hi
+```
 
--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,39 @@ use anyhow::{Context, Result};
 use std::fs;
 use std::os::unix::fs::chroot;
 use tempfile::TempDir;
-
+use std::path::Path;
+use nix::sys::stat::{mknod, Mode, SFlag};
 
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
 fn main() -> Result<()> {
     let args: Vec<_> = std::env::args().collect();
-    // let image = &args[2]; 
+    
+    // file system isolation
+    let tmp_dir = TempDir::new()?;
+    let bin_dir = tmp_dir.path().join("bin");
+    let dev_dir = tmp_dir.path().join("dev");
 
-    // here must create a temp dir and put the command in it
-    // then call chroot to this temp dir and then execute the 
-    // command as done bellow  
+    fs::create_dir_all(&bin_dir)?;
+    fs::create_dir_all(&dev_dir)?;
+
+    let source_path_echo = Path::new("/bin/echo");  // Replace with the actual source path
+    let source_path_null = Path::new("/dev/null");  // Replace with the actual source path
+
+    let target_path_echo = bin_dir.join("echo");
+    let target_path_null = dev_dir.join("null");
+
+    fs::copy(source_path_echo, target_path_echo)?;
+    // dev/null is a special type of node
+    let dev_null_mode = Mode::from_bits_truncate(0o666); // Permissions: rw-rw-rw-
+    mknod(&target_path_null, SFlag::S_IFCHR, dev_null_mode, nix::sys::stat::makedev(1, 3))
+        .expect("Failed to create /dev/null");
+
+    println!("Temporary directory created at: {:?}", tmp_dir.path());
+    //chroot
+    chroot("/")?;
+    //chroot(tmp_dir.path())?;
+    std::env::set_current_dir("/")?;
+
     
 
     let command = &args[3];
@@ -25,7 +48,7 @@ fn main() -> Result<()> {
                 command, command_args
             )
         })?;
-    
+    unsafe { libc::unshare(libc::CLONE_NEWPID) };
     if output.status.success() {
         let std_out = std::str::from_utf8(&output.stdout)?;
         print!("{}", std_out);
