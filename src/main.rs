b40a5469991674b14e520705444148d053a5932f
Florian Garbe <fpgarbe.pro@gmail.com>
2025-01-27 16:21:01 +0100

    creating temp dir & copying command to it & adding context to errors

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use anyhow::{Context, Result, anyhow};
-use std::fs;
+use std::fs::{copy, create_dir_all};
 use std::os::unix::fs::chroot;
 use tempfile::TempDir;
 use std::path::Path;
@@ -7,44 +7,56 @@ use std::env;
 
 
 // Usage: your_docker.sh run <image> <command> <arg1> <arg2> ...
-// fn main() -> Result<()> {
-//     let args: Vec<_> = std::env::args().collect();
-//     // let image = &args[2]; 
-// 
-//     // here must create a temp dir and put the command in it
-//     // then call chroot to this temp dir and then execute the 
-//     // command as done bellow  
-//     
-// 
-//     let command = &args[3];
-//     let command_args = &args[4..];
-//     let output = std::process::Command::new(command)
-//         .args(command_args)
-//         .output()
-//         .with_context(|| {
-//             format!(
-//                 "Tried to run '{}' with arguments {:?}",
-//                 command, command_args
-//             )
-//         })?;
-//     
-//     if output.status.success() {
-//         let std_out = std::str::from_utf8(&output.stdout)?;
-//         print!("{}", std_out);
-// 	let std_err = std::str::from_utf8(&output.stderr)?;
-// 	eprint!("{}", std_err);
-//     } else {
-//         let exit_code = output.status.code().unwrap_or(-1);
-// 	if exit_code==-1 {
-// 		std::process::exit(0);
-// 	} else {
-// 		println!("exit with code {}",  exit_code);
-// 		std::process::exit(exit_code);
-// 	}
-//     }
-// 
-//     Ok(())
-// }
+fn main() -> Result<()> {
+    let args: Vec<_> = std::env::args().collect();
+    // let image = &args[2]; 
+
+    // here must create a temp dir and put the command in it
+    // then call chroot to this temp dir and then execute the 
+    // command as done bellow  
+
+    let command = &args[3];
+    let command_args = &args[4..];
+
+	let tmp_dir = TempDir::new()?;
+	create_dir_all(tmp_dir.path().join("dev/null"))
+		.context("failed in creating null device")?;
+	let dst = tmp_dir
+		.path()
+		.join(command.strip_prefix("/").unwrap_or(command));
+
+// 	println!("the new location of the command is \n:{}", dst.to_string_lossy().to_string());
+
+	let resolved = resolve_name(command).context("failed to resolving name of command")?;
+	copy(resolved, dst).context("failed to copy")?;	
+
+    let output = std::process::Command::new(command)
+        .args(command_args)
+        .output()
+        .with_context(|| {
+            format!(
+                "Tried to run '{}' with arguments {:?}",
+                command, command_args
+            )
+        })?;
+    
+    if output.status.success() {
+        let std_out = std::str::from_utf8(&output.stdout)?;
+        print!("{}", std_out);
+	let std_err = std::str::from_utf8(&output.stderr)?;
+	eprint!("{}", std_err);
+    } else {
+        let exit_code = output.status.code().unwrap_or(-1);
+	if exit_code==-1 {
+		std::process::exit(0);
+	} else {
+		println!("exit with code {}",  exit_code);
+		std::process::exit(exit_code);
+	}
+    }
+
+    Ok(())
+}
 
 fn resolve_name(command: &str) -> Result<String> {
     if Path::new(command).is_absolute() || command.contains('/') {
@@ -65,8 +77,8 @@ fn resolve_name(command: &str) -> Result<String> {
 	return Err(anyhow!("error resolving path"));
 }
 
-fn main() -> Result<()> {
-	let a = resolve_name("ls")?;
-	println!("the resolved name is: {}", a);
-	Ok(())
-}
+// fn main() -> Result<()> {
+// 	let a = resolve_name("ls")?;
+// 	println!("the resolved name is: {}", a);
+// 	Ok(())
+// }
